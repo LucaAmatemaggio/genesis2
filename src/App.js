10d26f96518f32efd253cfb8128afe32f0faab86
Amatemaggio Luca <luca.amatemaggio@boschrexroth.it>
2021-04-13 15:48:44 +0200

    test for git

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Test for CI/CD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
